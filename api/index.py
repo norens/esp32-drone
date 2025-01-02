dbeb3debe0dc2b6dbba3beadb674c57820c81308
nazarf <nazarfedishin@gmail.com>
2025-01-02 19:53:45 +0100

    [API] Add set trim, drone pid, point

--- api/index.py
+++ api/index.py
@@ -1,9 +1,10 @@
+import json
+import threading
+import time
+
+import serial
 from flask import Flask
 from flask_socketio import SocketIO
-import threading
-import serial
-import json
-import time
 
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins='*')
@@ -22,6 +23,38 @@ def arm_drone(data):
             ser.write(f"{str(droneIndex)}{json.dumps(serial_data)}".encode('utf-8'))
 
         time.sleep(0.01)
+
+@socketio.on("set-drone-setpoint")
+def arm_drone(data):
+    serial_data = {
+        "setpoint": [float(x) for x in data["droneSetpoint"]],
+    }
+    with serialLock:
+        ser.write(f"{str(data['droneIndex'])}{json.dumps(serial_data)}".encode('utf-8'))
+        time.sleep(0.01)
+    print(serial_data)
+
+
+@socketio.on("set-drone-trim")
+def arm_drone(data):
+    serial_data = {
+        "trim": [int(x) for x in data["droneTrim"]],
+    }
+    with serialLock:
+        ser.write(f"{str(data['droneIndex'])}{json.dumps(serial_data)}".encode('utf-8'))
+        time.sleep(0.01)
+    print(serial_data)
+
+
+@socketio.on("set-drone-pid")
+def arm_drone(data):
+    serial_data = {
+        "pid": [float(x) for x in data["dronePID"]],
+    }
+    with serialLock:
+        ser.write(f"{str(data['droneIndex'])}{json.dumps(serial_data)}".encode('utf-8'))
+        time.sleep(0.01)
+    print(serial_data)
 
 
 if __name__ == '__main__':
